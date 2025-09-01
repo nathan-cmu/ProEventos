07b177d8955f69fe2828f183b78429e5fc284c49
nathan <nathan.oliveira@cmuenergia.com.br>
2025-09-01 09:46:16 -0300

    feat: sec  1

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+
+        }
+        
+        public Evento Get()
+        {
+            return new Evento {
+                EventoId = 1,
+                Tema = "Angular e .NET",
+                Local = "Belo Horizonte",
+                Lote = "1º Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                ImagemURL = "foto.png"
+            };
+        }
+        
+        
+    }
+}
